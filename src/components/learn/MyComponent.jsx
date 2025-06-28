8ee067b92413964dc94f85aef7d186dca0911b6a
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-06-28 21:28:27 +0700

    Update JSX course

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,12 @@
+import './styles.css'
+
 const MyComponent = () => {
-  return <div>My Component</div>
+    return (
+        <>
+            <div>My Component</div>
+            <p className='child'>This is a simple component demonstrating React syntax.</p>
+        </>
+    )
 }
 
 export default MyComponent
