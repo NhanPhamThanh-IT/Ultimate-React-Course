1dbb32c4cd2c2d1c1c3df18cc73c48f2c230c248
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-06-29 08:35:38 +0700

    Update Variables in React course

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,29 @@
 import './styles.css'
 
 const MyComponent = () => {
+    const name = "NhanPham";
+    const arr = [1, 2, 3, 4, 5];
+    const obj = {
+        name: "NhanPham",
+        age: 25,
+    }
+
     return (
         <>
-            <div>My Component</div>
+            <div>{name}'s Component</div>
             <p className='child'>This is a simple component demonstrating React syntax.</p>
+            <ul>
+                {arr.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <div>
+                <p>Name: {obj.name}</p>
+                <p>Age: {obj.age}</p>
+            </div>
+            <div>
+                {JSON.stringify(obj, null, 2)}
+            </div>
         </>
     )
 }
