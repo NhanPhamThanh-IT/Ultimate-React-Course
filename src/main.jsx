8c0a1d2e90d15873bd0c6c5e5feadc5dc9ad62b3
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-07-29 22:15:33 +0700

    [Lessons] Integrated router in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <div>Login Page</div>
+  },
+  {
+    path: '/register',
+    element: <div>Register Page</div>
+  },
+  {
+    path: '/users',
+    element: <div>Users Page</div>
+  },
+  {
+    path: '/products',
+    element: <div>Products Page</div>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
